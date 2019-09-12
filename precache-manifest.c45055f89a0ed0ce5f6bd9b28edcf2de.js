0edb7a1a2100602b0837b5102f5db2373ba2493b
jsunga <jawyn.sunga@gmail.com>
2019-09-11 22:50:16 -0700

    Updates

---
+++ precache-manifest.c45055f89a0ed0ce5f6bd9b28edcf2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16a690f2fb4721fe1c375dba94826c0",
+    "url": "/Foodie-Land/index.html"
+  },
+  {
+    "revision": "d99756c9adfc4da40a3f",
+    "url": "/Foodie-Land/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "4890dea43eb09696a5e3",
+    "url": "/Foodie-Land/static/css/main.53fbf715.chunk.css"
+  },
+  {
+    "revision": "d99756c9adfc4da40a3f",
+    "url": "/Foodie-Land/static/js/2.fb3ef7f0.chunk.js"
+  },
+  {
+    "revision": "4890dea43eb09696a5e3",
+    "url": "/Foodie-Land/static/js/main.f5149f7c.chunk.js"
+  },
+  {
+    "revision": "3f540213d5c1f56011a3",
+    "url": "/Foodie-Land/static/js/runtime~main.8bc56bc3.js"
+  }
+]);
